c53384fbb472d308a500ed5bc56e591197a337de
l-arkadiy-l <72223169+l-arkadiy-l@users.noreply.github.com>
2020-10-07 17:48:53 +0300

    Update main.py

--- Текстовый редактор 1.0/main.py
+++ Текстовый редактор 1.0/main.py
@@ -11,22 +11,33 @@ class Main(QMainWindow, Ui_MainWindow):
         self.setup()
 
     def setup(self):
-        self.pushButton_create.clicked.connect(self.create_save_file)
-        self.pushButton_open.clicked.connect(self.open_file)
-        self.pushButton_save.clicked.connect(self.create_save_file)
-
-    def create_save_file(self):
-        if self.lineEdit.text():
-            title = self.lineEdit.text()
-            text = self.texteidt.toPlainText()
-            with open(title, 'w') as f:
-                f.write(text)
+        self.pushButton.clicked.connect(self.open_file)
+        self.pushButton_2.clicked.connect(self.save_file)
+        self.pushButton_3.clicked.connect(self.create_file)
 
     def open_file(self):
-        if self.lineEdit.text():
-            title = self.lineEdit.text()
-            with open(title) as f:
-                self.texteidt.setPlainText(f.read())
+        file = self.lineEdit.text()
+        try:
+            with open(file) as f:
+                self.plainTextEdit.setPlainText(f.read())
+            self.label.setText('Редактор файлов')
+        except Exception:
+            self.label.setText('Такой файл не найден')
+
+    def save_file(self):
+        file = self.lineEdit.text()
+        try:
+            open(file)
+            with open(file, 'w') as f:
+                f.write(self.plainTextEdit.toPlainText())
+        except Exception:
+            self.label.setText('Такой файл не найден')
+
+    def create_file(self):
+        file = self.lineEdit.text()
+        with open(file, 'w') as f:
+            f.write(self.plainTextEdit.toPlainText())
+        self.label.setText('Редактор файлов')
 
 
 if __name__ == '__main__':
