c25ed39e6a8ed911f579f64e0c7fc5f435ff6299
|-arkadiy-| <arkadiy.kleymenov11@yandex.ru>
2020-12-26 16:22:28 +0300

    add mario game 1

---
+++ mini pygame projects/mario/main.py
@@ -0,0 +1,131 @@
+import pygame as pg
+
+FILEDIR = 'data'
+tile_images = {
+    'wall': pg.image.load('{}/box.png'.format(FILEDIR)),
+    'empty': pg.image.load('{}/grass.png'.format(FILEDIR))
+}
+player_image = pg.image.load('{}/mario.png'.format(FILEDIR))
+
+tile_width = tile_height = 50
+player = None
+
+# группы спрайтов
+all_sprites = pg.sprite.Group()
+tiles_group = pg.sprite.Group()
+player_group = pg.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y, True)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+class Tile(pg.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y, is_wall=False):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.is_wall = is_wall
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.speed = 50
+        self.vel = pg.math.Vector2((0, 0))
+
+    def move(self, event):
+        if event.key == pg.K_DOWN:
+            self.rect.y += self.speed
+            if pg.sprite.spritecollideany(self, tiles_group).is_wall:
+                self.rect.y -= self.speed
+        if event.key == pg.K_UP:
+            self.rect.y -= self.speed
+            if pg.sprite.spritecollideany(self, tiles_group).is_wall:
+                self.rect.y += self.speed
+        if event.key == pg.K_RIGHT:
+            self.rect.x += self.speed
+            if pg.sprite.spritecollideany(self, tiles_group).is_wall:
+                self.rect.x -= self.speed
+        if event.key == pg.K_LEFT:
+            self.rect.x -= self.speed
+            if pg.sprite.spritecollideany(self, tiles_group).is_wall:
+                self.rect.x += self.speed
+
+
+def start_screen(filedir):
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pg.transform.scale(pg.image.load('{}/fon.jpg'.format(filedir)), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pg.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pg.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+
+def load_level(filename):
+    filename = FILEDIR + "/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+if __name__ == '__main__':
+    pg.init()
+    SIZE = WIDTH, HEIGHT = (550, 500)
+    screen = pg.display.set_mode(SIZE)
+    clock = pg.time.Clock()
+    running = True
+    draw_area = False
+    player, level_x, level_y = generate_level(load_level('map.txt'))
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                draw_area = True
+            if event.type == pg.KEYDOWN and draw_area:
+                player.move(event)
+        screen.fill(pg.Color('black'))
+        if not draw_area:
+            start_screen(FILEDIR)
+        else:
+
+            tiles_group.draw(screen)
+            # all_sprites.draw(screen)
+            player_group.draw(screen)
+
+        clock.tick(10)
+        pg.display.flip()
+    pg.quit()
--- mini pygame projects/maybe mario.py
+++ mini pygame projects/maybe mario.py
@@ -20,27 +20,48 @@ class Hero(pg.sprite.Sprite):
         self.rect.x = self.pos[0]
         self.rect.y = self.pos[-1]
 
-    def move_hero(self, event):
+    def move_hero(self, event, group_stairs):
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_RIGHT:
-                self.vel.x += self.speed
+                self.vel.x = self.speed
             if event.key == pg.K_LEFT:
-                self.vel.x -= self.speed
+                self.vel.x = -self.speed
+            self.update()
+            s = pg.sprite.spritecollideany(self, group_stairs)
+            # БАГ
+            # персонаж, если зажать кнопку вверхи или вниз улетает
+            if s:
+                if event.key == pg.K_UP:
+                    self.vel.y = -self.speed
+                if event.key == pg.K_DOWN:
+                    self.vel.y = self.speed
         elif event.type == pg.KEYUP:
             if event.key == pg.K_RIGHT:
                 self.vel.x = 0
             if event.key == pg.K_LEFT:
                 self.vel.x = 0
+            if event.key == pg.K_UP:
+                self.vel.y = 0
+            if event.key == pg.K_DOWN:
+                self.vel.y = 0
 
 
 class Floor(pg.sprite.Sprite):
     def __init__(self, pos, *group):
         super(Floor, self).__init__(*group)
         self.image = pg.Surface((50, 10))
+        self.image.fill(pg.Color('gray'))
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[-1]
+
+
+class Stair(pg.sprite.Sprite):
+    def __init__(self, pos, *group):
+        super(Stair, self).__init__(*group)
+        self.image = pg.Surface((10, 50))
         self.image.fill(pg.Color('red'))
         self.rect = self.image.get_rect()
-        print(self.rect.width)
-        self.pos = pg.math.Vector2(pos)
         self.rect.x = pos[0]
         self.rect.y = pos[-1]
 
@@ -57,6 +78,7 @@ if __name__ == '__main__':
     SIZE = WIDTH, HEIGHT = (500, 500)
     screen = pg.display.set_mode(SIZE)
     group_floors = pg.sprite.Group()
+    group_stairs = pg.sprite.Group()
     running = True
     hero = Hero((0, 0), False)
     clock = pg.time.Clock()
@@ -66,20 +88,28 @@ if __name__ == '__main__':
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 3:
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 3 and pg.key.get_mods() & pg.KMOD_CTRL:
+                print('ok')
+                group_stairs.add(Stair(event.pos))
+            elif event.type == pg.MOUSEBUTTONDOWN and event.button == 3:
                 hero.create = True
                 hero.pos = event.pos
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
                 floor = Floor(event.pos)
                 group_floors.add(floor)
-            hero.move_hero(event)
+
+            hero.move_hero(event, group_stairs)
         screen.fill(pg.Color('black'))
         sp = pg.sprite.spritecollideany(hero, group_floors)
+        collide_stair = pg.sprite.spritecollideany(hero, group_stairs)
         group_floors.draw(screen)
+        group_stairs.draw(screen)
         if hero.create:
             hero.update()
             screen.blit(hero.image, hero.pos)
-        if not sp:
+        if not sp and not collide_stair:
             hero.pos = subtraction_tuples(hero.pos, (0, 3))
+        hero.rect.x = hero.pos[0]
+        hero.rect.y = hero.pos[-1]
         clock.tick(fps)
         pg.display.flip()
